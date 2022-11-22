8fa8cddcd6c50847a55b91ce2b38944be0decee8
jestemboski999 <jestemboski999@gmail.com>
2022-11-22 13:08:02 +0100

    V1

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -2,8 +2,27 @@
 
 #include "Stocznia.hpp"
 
+int CargoSum = 0;
+int temp = 0;
+int sailingShipCounter = 0;
+
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+Stocznia fabryka{};
+
+    while(CargoSum < towar)
+    {
+        Statek* s1 = fabryka();
+        temp = s1->transportuj();
+        CargoSum = CargoSum + temp;
+
+        if( dynamic_cast<Zaglowiec*>(s1) != nullptr )
+        {
+            sailingShipCounter++;
+        }
+
+        delete s1;
+    }
+
+    return sailingShipCounter;
 }
